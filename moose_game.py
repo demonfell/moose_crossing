41220714c99a15793e41b3a480c4e169bfe1921b
Jamie Pemantell <pbin2au@gmail.com>
2017-10-09 18:19:04 -0400

    Created a while loop for user input, but finding a lot of bugs

--- moose_game.py
+++ moose_game.py
@@ -20,12 +20,14 @@ def check_dbl(current_guess,puz_in_prog,bad_guesses):
     """
     Checks to see if you already guessed a letter.
     """
-    if letter in puz_in_prog:
-        print("You already guessed " + letter + "!")
+    if current_guess in puz_in_prog:
+        print("You already guessed " + current_guess + "!")
         return True
-    if letter in bad_guesses:
-        print("You already guessed " + letter + "!") 
+    if current_guess in bad_guesses:
+        print("You already guessed " + current_guess + "!") 
         return True 
+    else:
+        return False
 
 # Define variables
 # For the earliest version, define the word list here
@@ -39,17 +41,17 @@ chosen_puzzle=random.choice(word_list)
 #old
 #chosen_puzzle=random.choice(word_list)
 #while True:
-#	my_guess = raw_input ("Enter your guess: ")
-#	saved_guesses.append(my_guess)
-#	for i in chosen_puzzle:
-#		for j in saved_guesses:
-#			if j in i:
-#				print(i) , 
-#				break;
-#			else:
-#				print "_ " ,
+#   my_guess = raw_input ("Enter your guess: ")
+#   saved_guesses.append(my_guess)
+#   for i in chosen_puzzle:
+#       for j in saved_guesses:
+#           if j in i:
+#               print(i) , 
+#               break;
+#           else:
+#               print "_ " ,
 
-#Initialize the saved guesses array with placemarker underscores		 
+#Initialize the saved guesses array with placemarker underscores         
 #was not working, 10/7/2017
 #for ctr in range(len(chosen_puzzle)):
 #   if chosen_puzzle[ctr] != ' ':
@@ -61,27 +63,36 @@ chosen_puzzle=random.choice(word_list)
 for letter in chosen_puzzle:
    ctr=0
    if letter != ' ':
-       saved_guesses.insert(counter,"_ "),
+       saved_guesses.insert(ctr,"_ "),
        ctr+=1
    elif letter == ' ':
-       saved_guesses.insert(counter," "),
+       saved_guesses.insert(ctr," "),
        ctr+=1
 
-my_guess = input("Enter your guess: ")
+
+prompt = "\n Enter a letter to guess the word puzzle"
+prompt += "\n Enter 'quit' to end the program   > "
+my_guess = ""
 
 #main guessing loop
-matched = 0                                          
-for num,letter in enumerate(chosen_puzzle, start=0):
-    if check_dbl(my_guess,saved_guesses,wrong_guesses):
-        break
-#    print("Letter {}: {}".format(num, letter))
-    elif letter == my_guess:                        
-        saved_guesses[num] = my_guess
-        matched+=1           
+while my_guess != 'quit':
+    matched = 0  
+    my_guess = (input(prompt)).lower()
+    print(saved_guesses)                                 
+    for num,letter in enumerate(chosen_puzzle, start=0):
+#       print("Letter {}: {}".format(num, letter))
+        if check_dbl(my_guess,saved_guesses,wrong_guesses):
+            print(my_guess)
+            break  
+
+        elif letter == my_guess:                        
+            saved_guesses[num] = my_guess
+            matched+=1           
                                                      
-    elif letter != my_guess and num == (len(chosen_puzzle)-1) and matched == 0:
-        print("Wrong guess")
-        wrong_guesses.append(letter)
+        elif letter != my_guess and num == (len(chosen_puzzle)-1) and matched == 0:
+            print("Wrong guess")
+            wrong_guesses.append(my_guess)
+            print("You have made " + str(len(wrong_guesses)) + " wrong guesses.")
 
 
 
