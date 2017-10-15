35dba76a25c2968462d16d3541bca248be743b3a
Jamie Pemantell <pbin2au@gmail.com>
2017-10-14 21:13:08 -0400

    Added the dictionary with glossary terms. Implemented into check_fin function to dispaly when puzzle is complete.

--- moose_game.py
+++ moose_game.py
@@ -6,14 +6,14 @@ from numpy import array_equal
 
 #Draft of function - 10/9
 
-def check_fin(puz_in_prog,orig_puz):
+def check_fin(puz_in_prog,orig_puz,definitions):
     """
     Checks a puzzle in progress to see if it's finished
     """
     if array_equal(''.join(puz_in_prog),orig_puz):
         print("You have completed the puzzle!")
-        print(puz_in_prog)
         print(orig_puz)
+        print(definitions[orig_puz])
         return True
     else:
         return False
@@ -42,13 +42,14 @@ def check_dbl(current_guess,puz_in_prog,bad_guesses):
 
 # Define variables
 # For the earliest version, define the word list here
-word_list=['computer', 'memory', 'disk space', 'monitor', 'video card']
+word_list=['processor', 'memory', 'disk drive', 'monitor', 'display adapter']
+
 saved_guesses = []
 wrong_guesses = []
 
 #randomly choose a word from the list for the puzzle
 chosen_puzzle=random.choice(word_list)
-
+term_defs={"Processor":"The computing part of the computer. Also called the CPU (central processing unit),it is made up of the control unit and ALU (Arithmetic Logic Unit). Today, the CPUs of almost all computers are contained on a single microprocessor chip.","memory":"The computer's temporary workspace, a collection of dynamic RAM (DRAM) chips. A major resource in the computer, memory determines the size and number of programs that can be run at the same time, as well as the amount of data that can be processed instantly. All program execution and data processing takes place in memory.","monitor":"A display screen used to provide visual output from a computer. Computer monitors use CRT (cathode ray tube) and LCD (liquid crystal display) technology.", "display adapter":"A plug-in card in a desktop computer that performs graphics processing. Also commonly called a graphics card or video card, modern display adapters use the PCI Express interface, while earlier cards used PCI and AGP. The display adapter determines the maximum resolution, refresh rate and number of colors that can be displayed, which the monitor must also be able to support. On many PC motherboards, the graphics circuits are built into the chipset, and a separate plug-in card is not required.", "disk drive":"A storage device that holds, spins, reads and writes magnetic disks or optical discs." }
 #Initialize the saved guesses array with placemarker underscores        
 for letter in chosen_puzzle:
    ctr=0
@@ -91,7 +92,7 @@ while my_guess != 'quit':
             print("You have made " + str(len(wrong_guesses)) + " wrong guesses.")
             print(' '.join(saved_guesses))
 
-        elif num == (len(chosen_puzzle)-1) and check_fin(saved_guesses,chosen_puzzle):
+        elif num == (len(chosen_puzzle)-1) and check_fin(saved_guesses,chosen_puzzle,term_defs):
             print("Goodbye! Thanks for playing.")
             break
 
